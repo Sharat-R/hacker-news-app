8f37bff4bcdca187cdec9f728ebe16bc8a0e2b04
codispatch <codispatch.shravan.c@gmail.com>
2021-06-19 17:19:56 +0530

    -- added pagination component

---
+++ src/Pagination/Pagination.js
@@ -0,0 +1,11 @@
+import "./Pagination.css";
+
+function Pagination () {
+
+    return (<div className="pagination">
+        <button>Previous</button> 
+        <button>Next</button> 
+       </div> )
+}
+
+export default Pagination;
